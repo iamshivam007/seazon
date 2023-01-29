27218a18f91702f4f48e7aa6d47a9a3879c62dee
Shivam Jindal <jindalshivam65@gmail.com>
2023-01-29 22:14:45 +0530

    Add db creds in env template

--- seazon/settings.py
+++ seazon/settings.py
@@ -161,15 +161,6 @@ elif os.environ.get('use_aws'):
             'PORT': os.environ.get('DB_PORT'),
         }
     }
-    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-    AWS_DEFAULT_ACL = None
-    AWS_S3_CUSTOM_DOMAIN = '{}.s3.amazonaws.com'.format(AWS_STORAGE_BUCKET_NAME)
-    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
-    # s3 private media settings
-    PRIVATE_MEDIA_LOCATION = 'private_media'
-    DEFAULT_FILE_STORAGE = 'mocktest.utils.PrivateMediaStorage'
 
 else:
     # Database
